0d59f5f131d6721317bacc593bd4bc10fee7f749
KEKE Yannick <kekeyan200211@gmail.com>
2025-03-08 11:31:32 +0100

    feat: Using env variables

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,17 @@ from flask_migrate import Migrate
 from app.models import db
 from flask_jwt_extended import JWTManager
 from app.routes import auth_bp
+from dotenv import load_dotenv
+import os
 
 def create_app():
+    load_dotenv()
+
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://yann:password@db/test'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'
+    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+
     db.init_app(app)
     Migrate(app, db)
     jwt = JWTManager(app)
